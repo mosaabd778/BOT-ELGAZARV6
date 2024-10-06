902d692b5a9fcd59bebad7d70838625fd0c92f54
mosaabd778 <mosaabdalslamali@gmail.com>
2024-10-06 21:33:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['+967778137625'] //ur owner number
+global.ownername = "شُـأّدٍوٌ 影❤️" //ur owner name
+global.ytname = "YT: شُـأّدٍوٌ 影" //ur yt chanel name
+global.socialm = "GitHub: شُـأّدٍوٌ 影" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "+967778137625" //ur number
+global.premium = ['+967778137625'] //ur premium number
+global.botname = '❤️شُـأّدٍوٌ 影 ᗷOT❤️' //ur bot name
+global.linkz = "+967778137625" //your theme url which will be displayed on whatsapp
+global.websitex = "+967778137625" //ur website to be displayed
+global.botscript = '+967778137625' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣شُـأّدٍوٌ 影 الوزير يروحي⍣" //ur sticker watermark packname
+global.author = "⍣شُـأّدٍوٌ 影⍣" //ur sticker watermark author
+global.wm = "❤️شُـأّدٍوٌ 影 ᗷOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
